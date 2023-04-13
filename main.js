18ae8048df38a9055618f33d7170c5e567b38140
rain72510 <rain72510@gmail.com>
2023-04-14 05:13:05 +0800

    Can play with rectngle

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ var mode = init_mode; // 0: cursor, 1: line, 2: rectangle, 3: ellipse
 var border_color = init_border_color; // 1:"#fff", 2: "#999", 3:"#000", 4:"#ff0", 5:"#f00", 6:"#0ff", 7:"#0f0"
 var border_width = init_border_width;
 var fill_color = init_fill_color; // 1:"#fff", 2: "#999", 3:"#000", 4:"#ff0", 5:"#f00", 6:"#0ff", 7:"#0f0"
+
 var mousedowned = false;
 var mousedown_cursorX = 0;
 var mousedown_cursorY = 0;
@@ -19,65 +20,53 @@ var mousemove_cursorX = 0;
 var mousemove_cursorY = 0;
 var mouseup_cursorX = 0;
 var mouseup_cursorY = 0;
-var obj_id = 0;
+var obj_id_cnt = 0;
+var current_obj = null;
 
 
-const colors = {1:"#fff", 2: "#999", 3:"#000", 4:"#ff0", 5:"#f00", 6:"#0ff", 7:"#0f0"}
-const obj_dict = {}
+const colors = {1:"#fff", 2: "#999", 3:"#000", 4:"#ff0", 5:"#f00", 6:"#0ff", 7:"#0f0"};
+const obj_dict = {};
 
-const draw_rectangle = (fill_color, stroke_width, stroke_color, width, height, x, y, opts) => {
+const createStyle = (stroke_color, stroke_width, fill_color) => {
+  return `
+  fill:${colors[fill_color]};
+  stroke-width:${stroke_width}px;
+  stroke:${colors[stroke_color]}`
+}
+
+const drawRectangle = (stroke_color, stroke_width, fill_color, width, height, x, y, opts) => {
   return `
   <rect`
   + (opts["id"]?` id="${opts["id"]}"`:"") +
   `
   x=${x} y=${y}
   width=${width}px height=${height}px
-  style="fill:${fill_color};
-  stroke-width:${stroke_width}px;
-  stroke:${stroke_color}"
+  style="${createStyle(stroke_color, stroke_width, fill_color)}"
   shape-rendering="crispEdges"
   />`;
 }
 
-const draw_ellipse = (fill_color, stroke_width, stroke_color, rx, ry, cx, cy) => {
-  return `\
-  <ellipse\
-  cx=${cx} cy=${cy}\
-  rx=${rx}px ry=${ry}px\
-  style="fill:${fill_color};\
-  stroke-width:${stroke_width}px;\
-  stroke:${stroke_color}"\
-  shape-rendering="crispEdges"\
+const drawEllipse = (stroke_color, stroke_width, fill_color, rx, ry, cx, cy) => {
+  return `
+  <ellipse
+  cx=${cx} cy=${cy}
+  rx=${rx}px ry=${ry}px
+  style="${createStyle(stroke_color, stroke_width, fill_color)}"
+  shape-rendering="crispEdges"
   />`;
 }
 
 const loadingPanel = () => {
-  var modeInputs = document.getElementById("modeInput");
   var borderColorInputs = document.getElementById("borderColorInput");
   var fillColorInputs = document.getElementById("fillColorInput");
-  modeInputs.innerHTML += `
-    <label class="inputIcon flexbox" >
-      <input id="modeInput2" type="button" onclick="selectPanel(event)">
-      <svg id="modeInput2Icon" viewbox="0, 0, 42, 28">
-        ${draw_rectangle(colors[init_fill_color], 1, colors[init_border_color], 42, 28, 0, 0, {})}
-      </svg>
-    </label>
-    `;
-  modeInputs.innerHTML += `
-    <label class="inputIcon flexbox" >
-      <input id="modeInput3" type="button" onclick="selectPanel(event)">
-      <svg id="modeInput3Icon" viewbox="0, 0, 42, 28">
-        ${draw_ellipse(colors[init_fill_color], 1, colors[init_border_color], 21, 14, 21, 14, {})}
-      </svg>
-    </label>
-    `;
+  renderModeInputIcon();
   for (let v = 1; v < Object.keys(colors).length + 1; v++) {
     // console.log(v);
     borderColorInputs.innerHTML += `\
     <label class="inputIcon flexbox">\
       <input id="borderColorInput${v}" type="button" onclick="selectPanel(event)">\
       <svg viewbox="0, 0, 42, 28">\
-        ${draw_rectangle(colors[v], 1, colors[3], 42, 28, 0, 0, {})}\
+        ${drawRectangle(3, 1, v, 42, 28, 0, 0, {})}\
       </svg>\
     </label>\
     `;
@@ -88,23 +77,70 @@ const loadingPanel = () => {
     <label class="inputIcon flexbox">\
       <input id="fillColorInput${v}" type="button" onclick="selectPanel(event)">\
       <svg viewbox="0, 0, 42, 28">\
-        ${draw_rectangle(colors[v], 1, colors[3], 42, 28, 0, 0, {})}\
+        ${drawRectangle(3, 1, v, 42, 28, 0, 0, {})}\
       </svg>\
     </label>\
     `;
   }
-  document.getElementById(`borderColorInput${init_border_color}`).parentElement.style.border="solid 1px #00f";
-  document.getElementById(`fillColorInput${init_fill_color}`).parentElement.style.border="solid 1px #00f";
-  document.getElementById(`modeInput${init_mode}`).parentElement.style.border="solid 1px #00f";
+  document.getElementById(`borderColorInput${border_color}`).parentElement.style.border="solid 1px #00f";
+  document.getElementById(`fillColorInput${fill_color}`).parentElement.style.border="solid 1px #00f";
+  document.getElementById(`modeInput${mode}`).parentElement.style.border="solid 1px #00f";
 }
 
-const change
+const renderModeInputIcon = () => { // also change border width
+  var modeInputs = document.getElementById("modeInput").parentElement.getElementsByTagName("label");
+  // console.log(modeInputs);
+  try {
+    var icon = document.getElementById("modeInput2Icon");
+    icon.remove();
+    var icon = document.getElementById("modeInput3Icon");
+    icon.remove();
+  } catch {};
+  modeInputs[2].innerHTML += `
+    <svg id="modeInput2Icon" viewbox="0, 0, 42, 28">
+      ${drawRectangle(border_color, 1, fill_color, 42, 28, 0, 0, {})}
+    </svg>
+    `;
+  modeInputs[3].innerHTML += `
+    <svg id="modeInput3Icon" viewbox="0, 0, 42, 28">
+      ${drawEllipse(border_color, 1, fill_color, 21, 14, 21, 14, {})}
+    </svg>
+    `;
+}
+
+const changePanelIconBorder = () => {
+  var modeInput = document.getElementById("modeInput");
+  for (let sib of modeInput.children) {
+    sib.style.border="solid 1px #fff";
+  }
+  document.getElementById(`modeInput${mode}`).parentElement.style.border="solid 1px #00f";
+  var borderColorInput = document.getElementById("borderColorInput");
+  for (let sib of borderColorInput.children) {
+    sib.style.border="solid 1px #fff";
+  }
+  document.getElementById(`borderColorInput${border_color}`).parentElement.style.border="solid 1px #00f";
+  var fillColorInput = document.getElementById("fillColorInput");
+  for (let sib of fillColorInput.children) {
+    sib.style.border="solid 1px #fff";
+  }
+  document.getElementById(`fillColorInput${fill_color}`).parentElement.style.border="solid 1px #00f";
+  var widthInput = document.getElementById("borderWidthInput0");
+  widthInput.parentElement.getElementsByTagName("p")[0].innerHTML = border_width;
+  widthInput.value = border_width;
+}
 
 const loadingWorkspace = () => {
-  var workpace = document.querySelector(".workspace");
-  workpace.addEventListener("mousedown", mousedown);
-  workpace.addEventListener("mousemove", mousemove);
-  workpace.addEventListener("mouseup", mouseup);
+  var workspace = document.querySelector(".workspace");
+  workspace.addEventListener("mousedown", mousedown);
+  workspace.addEventListener("mousemove", mousemove);
+  workspace.addEventListener("mouseup", mouseup);
+  svg.innerHTML += `
+  <filter id="selectFilter">
+    <feOffset result="offOut" in="SourceAlpha" dx="0" dy="0" />
+    <feGaussianBlur result="blurOut" in="offOut" stdDeviation="5" />
+    <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
+  </filter>
+  `
 }
 
 const printCurrentState = () => {
@@ -119,7 +155,7 @@ function draw_example() {
   var b = `<ellipse cx="300" cy="200" rx="100" ry="50"
   style="fill:yellow;stroke:purple;stroke-width:2" />`
   var c = `<rect x="0" y="0" width="50" height="20" style="fill:#0ff;  stroke-width:1px;  stroke:#000" shape-rendering="crispEdges"></rect>`
-  var d = draw_rectangle(colors[3], 1, colors[5], 100, 50, 200, 300, {});
+  var d = drawRectangle(colors[3], 1, colors[5], 100, 50, 200, 300, {});
   // svg.innerHTML += b;
   // svg.innerHTML += a;
   svg.innerHTML += d;
@@ -151,30 +187,40 @@ const selectLayer = () => { // 0: both, 1: svg, 2: pixelwise
 
 const selectPanel = (e) => {
   console.log("selectPanel !!")
-  for (let sib of e.target.parentElement.parentElement.children) {
-    sib.style.border="solid 1px #fff";
-  }
-  e.target.parentElement.style.border="solid 1px #00f";
   var id = e.target.getAttribute("id");
+  var chosenNum = parseInt(id.slice(-1))
   switch (id.slice(0, -1)) {
     case "modeInput":
-      mode = parseInt(id.slice(-1));
+      mode = chosenNum;
       break;
     case "borderColorInput":
-      border_color = parseInt(id.slice(-1));
+      border_color = chosenNum;
       break;
     case "fillColorInput":
-      fill_color = parseInt(id.slice(-1));
+      fill_color = chosenNum;
       break;
   }
+  modifyObj();
+  renderModeInputIcon();
+  changePanelIconBorder();
   printCurrentState();
 }
 
 const selectWidth = (e) => {
   var widthInput = document.getElementById("borderWidthInput0");
-  widthInput.parentElement.getElementsByTagName("p")[0].innerHTML = widthInput.value;
   border_width = widthInput.value;
-  // printCurrentState();
+  changePanelIconBorder();
+}
+
+const modifyObj = () => {
+  try {
+    current_obj.setAttribute("style", createStyle(border_color, border_width, fill_color));
+    var obj_info = obj_dict[`${current_obj.getAttribute("id")}`];
+    obj_info["border_color"] = border_color;
+    obj_info["border_width"] = border_width;
+    obj_info["fill_color"] = fill_color;
+    console.log(current_obj);
+  } catch {};
 }
 
 const mousedown = (e) => {
@@ -182,13 +228,16 @@ const mousedown = (e) => {
   mousedown_cursorY = e.clientY;
   console.log("mousedown, ", e.clientX, e.clientY);
   mousedowned = true;
+  if (mode == 0) {
+    chooseObj(e);
+  }
 }
 
 const mousemove = (e) => {
   mouseup_cursorX = e.clientX;
   mouseup_cursorY = e.clientY;
   if (mousedowned) {
-    draw_in_workspace(true);
+    drawInWorkspace(true);
   }
 }
 
@@ -196,17 +245,17 @@ const mouseup = (e) => {
   mouseup_cursorX = e.clientX;
   mouseup_cursorY = e.clientY;
   console.log("mouseup, ", e.clientX, e.clientY);
-  draw_in_workspace(false);
+  drawInWorkspace(false);
   mousedowned = false;
 }
 
-const draw_in_workspace = (tmp) => {
+const drawInWorkspace = (tmp) => {
   if (mode === 2) {
-    var id = `obj_id${obj_id}`;
+    var id = `obj_id${obj_id_cnt}`;
     var left = Math.min(mousedown_cursorX, mouseup_cursorX);
     var top = Math.min(mousedown_cursorY, mouseup_cursorY);
-    var rect = draw_rectangle(
-      colors[fill_color], border_width, colors[border_color],
+    var rect = drawRectangle(
+      border_color, border_width, fill_color,
       Math.abs(mouseup_cursorX - mousedown_cursorX),
       Math.abs(mouseup_cursorY - mousedown_cursorY),
       left - 260 - 8, top - 8,
@@ -220,8 +269,41 @@ const draw_in_workspace = (tmp) => {
       svg.innerHTML += rect;
     } else {
       svg.innerHTML += rect;
-      obj_id += 1;
+      obj_dict[`${id}`] = {
+        "mode": mode,
+        "border_color": border_color,
+        "border_width": border_width,
+        "fill_color": fill_color,
+        "width": Math.abs(mouseup_cursorX - mousedown_cursorX),
+        "height": Math.abs(mouseup_cursorY - mousedown_cursorY),
+        "x": left - 260 - 8,
+        "y": top - 8,
+      }
+      obj_id_cnt += 1;
     }
     
+  }
+}
+
+const chooseObj = (e) => {
+  console.log("chooseObj !", e.target);
+  var tar = e.target;
+  try {
+    current_obj.setAttribute("filter", "");
+  } catch {};
+  if (tar.getAttribute("id").slice(0, 6) == "obj_id") {
+    tar.setAttribute("filter", `url(#selectFilter)`);
+    current_obj = tar;
+    var obj_info = obj_dict[`${current_obj.getAttribute("id")}`];
+    console.log(obj_info);
+    border_color = obj_info["border_color"];
+    border_width = obj_info["border_width"];
+    fill_color = obj_info["fill_color"];
+    renderModeInputIcon();
+    changePanelIconBorder();
+  }
+
+  if (tar.getAttribute("id") == "svg") {
+    current_obj = null;
   }
 }
